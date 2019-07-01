d9b33c6f4766a0b827f3ffbda80ec4916c93d4f4
ljw1412 <506644791@qq.com>
2019-07-01 22:38:56 +0800

    add input.

--- src/views/Main/Personal/PasswordCard.vue
+++ src/views/Main/Personal/PasswordCard.vue
@@ -3,10 +3,20 @@
     shadow="hover"
     :body-style="{padding:0, height:'100%'}">
     <span>密码修改</span>
-    <el-form :model="user">
-      <el-form-item label="当前密码"></el-form-item>
-      <el-form-item label="修改密码"></el-form-item>
-      <el-form-item label="确认密码"></el-form-item>
+    <el-form :model="user"
+      label-width="80px">
+      <el-form-item label="当前密码">
+        <el-input v-model="user.password"
+          placeholder="请输入当前密码"></el-input>
+      </el-form-item>
+      <el-form-item label="修改密码">
+        <el-input v-model="user.modifyPassword"
+          placeholder="请输入修改密码"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码">
+        <el-input v-model="user.confirmPassword"
+          placeholder="请再次输入修改密码"></el-input>
+      </el-form-item>
     </el-form>
   </el-card>
 </template>

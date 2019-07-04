6aa055e668cf0a7801d9100e1a180deca95a2418
ljw1412 <506644791@qq.com>
2019-07-04 18:31:36 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,28 @@
+const webpack = require('webpack')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
   devServer: {
+    compress: true,
     port: 8998
+  },
+  configureWebpack: {
+    //警告 webpack 的性能提示
+    performance: {
+      hints: 'warning',
+      //入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      //生成文件的最大体积
+      maxAssetSize: 30000000,
+      //只给出 js 文件的性能提示
+      assetFilter: function(assetFilename) {
+        return assetFilename.endsWith('.js')
+      }
+    },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: `Builder: ljw1412\nBuild time: ${new Date()}`
+      })
+    ]
   }
 }

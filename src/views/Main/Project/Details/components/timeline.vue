b0777318b0e572af164b62bdb7e15c4d51044295
ljw1412 <506644791@qq.com>
2019-08-22 16:29:10 +0800

    add user.

--- src/views/Main/Project/Details/components/timeline.vue
+++ src/views/Main/Project/Details/components/timeline.vue
@@ -2,8 +2,12 @@
   <div class="timeline">
     <el-timeline>
       <el-timeline-item v-for="item of timelineList"
-        :key="item.id"
-        :timestamp="item.time">
+        hide-timestamp
+        :key="item.id">
+        <div class="timeline__time-and-user">
+          <span style="margin-right:10px;">{{item.time}}</span>
+          <span>{{item.user.nickname || item.user.username}}</span>
+        </div>
         <h3>{{item.title}}</h3>
         <p>{{item.log}}</p>
       </el-timeline-item>
@@ -32,11 +36,16 @@ export default {
         param: { id: this.project.id }
       }).then(data => {
         data.forEach(item => {
+          if (!item.user) item.user = {}
           item.time = moment(item.unix).format('YYYY-MM-DD HH:mm:ss')
         })
         this.timelineList = data
       })
     }
+  },
+
+  created() {
+    this.reFindTimelineList()
   },
 
   watch: {
@@ -50,7 +59,15 @@ export default {
 <style lang="scss" scoped>
 @import '@/theme/index.scss';
 .timeline {
+  box-sizing: border-box;
   @include auto-center-view(1000px);
   padding: 20px 15px 0 15px;
+  &__time-and-user {
+    margin-bottom: 8px;
+    padding-top: 4px;
+    color: #909399;
+    line-height: 1;
+    font-size: 13px;
+  }
 }
 </style>

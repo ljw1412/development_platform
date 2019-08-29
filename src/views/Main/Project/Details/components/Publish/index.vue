e0221a0607ed5ea44fdf38239d253a925fa6c615
ljw1412 <506644791@qq.com>
2019-08-29 11:43:54 +0800

    publish flows

--- src/views/Main/Project/Details/components/Publish/index.vue
+++ src/views/Main/Project/Details/components/Publish/index.vue
@@ -4,20 +4,34 @@
     <template v-if="publishList.length">
       <div class="publish__action">
         <div>发布流程总数: {{publishList.length}}</div>
-        <div>
+        <div class="publish__icons">
           <el-link class="el-icon-plus"
             :underline="false"
             @click="onCreateClick" />
+          <el-link :class="isEdit?'el-icon-close':'el-icon-setting'"
+            :underline="false"
+            @click="isEdit = !isEdit" />
         </div>
       </div>
       <div class="publish__flows">
         <div v-for="item of publishList"
           :key="item.id"
-          class="flow-wrapper">
+          class="flow-wrapper"
+          :class="{ 'flow-wrapper--edit': isEdit }">
           <div class="flow"
-            :class="{'is-not-node':!item.nodeCount}"
+            :class="{ 'is-not-node': !item.nodeCount }"
             @click="onFlowCardClick(item)">
-            <span>{{item.name}}</span>
+            <div>{{item.name}}</div>
+            <div class="flow__options"
+              @click.stop>
+              <el-link class="el-icon-edit"
+                :underline="false"
+                @click="onEditFlowClick(item)" />
+              <el-link class="el-icon-delete"
+                type="danger"
+                :underline="false"
+                @click="onDeleteFlowClick(item)" />
+            </div>
           </div>
         </div>
       </div>
@@ -35,8 +49,17 @@
         </el-link>
       </template>
     </result>
+    <!-- 发布记录 -->
+    <el-table border
+      style="margin-top:10px;">
+      <el-table-column label="发布名称"></el-table-column>
+      <el-table-column label="信息"></el-table-column>
+      <el-table-column label="状态"></el-table-column>
+      <el-table-column label="操作"></el-table-column>
+    </el-table>
     <!-- 编辑抽屉 -->
-    <edit :visible.sync="isDisplayEdit"></edit>
+    <edit :visible.sync="isDisplayEdit"
+      :flow="currentFlow"></edit>
   </div>
 </template>
 
@@ -56,6 +79,7 @@ export default {
   data() {
     return {
       isLoaded: false,
+      isEdit: false,
       isDisplayEdit: false,
       publishList: [],
       currentFlow: {}
@@ -96,7 +120,6 @@ export default {
     },
 
     onFlowCardClick(item) {
-      this.currentFlow = item
       if (!item.nodeCount) {
         this.$confirm('该流程没有配置流程节点', '提示', {
           confirmButtonText: '立刻配置',
@@ -104,10 +127,26 @@ export default {
           type: 'warning'
         })
           .then(() => {
-            this.isDisplayEdit = true
+            this.onEditFlowClick(item)
           })
           .catch(() => {})
       }
+    },
+
+    onEditFlowClick(item) {
+      console.log(item)
+      this.currentFlow = item
+      this.isDisplayEdit = true
+    },
+
+    onDeleteFlowClick(item) {
+      this.$confirm(`确定要删除发布流程"${item.name}"吗？`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {})
+        .catch(() => {})
     }
   },
 
@@ -132,6 +171,12 @@ export default {
     padding: 10px;
     @include divider;
   }
+  &__icons {
+    * {
+      font-size: 18px;
+      margin: 0 5px;
+    }
+  }
   &__flows {
     display: flex;
     flex-wrap: wrap;
@@ -144,10 +189,21 @@ export default {
   min-width: 150px;
   padding: 5px;
   text-align: center;
+  &--edit {
+    .flow {
+      cursor: initial;
+    }
+    &:hover .flow__options {
+      display: block;
+    }
+  }
 }
 
 .flow {
   cursor: pointer;
+  user-select: none;
+  overflow: hidden;
+  position: relative;
   height: 50px;
   line-height: 50px;
   border: 1px solid $--border-color-base;
@@ -155,6 +211,20 @@ export default {
   &:not(.is-not-node):hover {
     transition-duration: 0.5s;
     background-color: $--border-color-extra-light;
+  }
+  &__options {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: none;
+    width: 100%;
+    height: 100%;
+    background-color: rgba($color: #000000, $alpha: 0.5);
+    a {
+      font-size: 20px;
+      margin: 0 5px;
+      color: #fff;
+    }
   }
 }
 
